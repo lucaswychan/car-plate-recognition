7cae0cbf5d78f960c574e64fab4602598b21ff18
LucasCwy <chanconan@gmail.com>
2024-06-24 14:58:05 +0800

    simply the ocr usage

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ from ocr.ocr_perform import ocr
 
 
 def main():
-    image_folder = "text_regions"
-    car_plate = ""
-    for file in natsorted(os.listdir(image_folder)):
-        image_path = os.path.join(image_folder, file)
-        text, confidence = ocr(image_path=image_path)
-        if confidence < 0.5:
-            text = "_"
-        car_plate += text
-
-    print(car_plate)
+    image_path = "image.png"
+    confidences, car_plate = ocr(image_path)
+    print(f"Recognized license plate: {car_plate}")
+    print(f"Confidence levels: {confidences}")
 
 
 if __name__ == "__main__":
