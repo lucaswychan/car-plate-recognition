44e5e43976abf9ef1955e9180f4a3e6656b4d494
LucasCwy <chanconan@gmail.com>
2024-06-24 15:03:29 +0800

    change the file path and remove unused modules

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import os
-
-from natsort import natsorted
-
 from ocr.ocr_perform import ocr
 
 
 def main():
-    image_path = "image.png"
+    image_path = "license_images/GV5679.png"
     confidences, car_plate = ocr(image_path)
     print(f"Recognized license plate: {car_plate}")
     print(f"Confidence levels: {confidences}")
